fce67d225234dfc73e7110cef93d5dce46d8309a
avto <avtoruxadze@gmail.com>
2015-11-30 01:33:17 +0400

    ddd

--- WebApplication2/Scripts/sbalerts.js
+++ WebApplication2/Scripts/sbalerts.js
@@ -1,3 +1,20 @@
 ﻿function sbconfirm(question, func) {
-    
+    var background = $('<div class="alertBackground"></div>');
+    var dialog = $('<div class="alertDialog">' +
+                       '<div style="margin-bottom: 15px">' +
+                        '<span class="glyphicon glyphicon-question-sign btn-lg" aria-hidden="true"></span>' +
+                        '<span style="font-size: 16px; font-weight: bold">გსურთ ოპერაციის შესრულება?</span>' +
+                         '</div>' +
+                        '<div>' +
+                        '<div class="btn btn-danger" style="float: right; margin-left: 15px;">'+
+                            '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +
+                                    არა
+</div>
+<div class="btn btn-success" style="float: right">
+    <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+    დიახ
+</div>
+</div>
+</div>');
+    $('body').append()
 }
